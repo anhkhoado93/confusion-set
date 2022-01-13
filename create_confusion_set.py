924a5960c43ecd3147c98b11a979c49655aad124
anhkhoado93 <45436284+anhkhoado93@users.noreply.github.com>
2022-01-14 03:33:45 +0700

    ok

--- create_confusion_set.py
+++ create_confusion_set.py
@@ -1,12 +1,14 @@
 from utils import *
 import pickle
-
+import os
 
 
 if __name__ == "__main__":
-
-    full_vocab = load_vocab("all-vietnamese-syllables.txt")
-    common_vocab = load_vocab("common-vietnamese-syllables.txt")
+    cwd = os.getcwd()
+    fvp = os.path.join(cwd, "all-vietnamese-syllables.txt")
+    fvp2 = os.path.join(cwd, "common-vietnamese-syllables.txt")
+    full_vocab = load_vocab(fvp)
+    common_vocab = load_vocab(fvp2)
     full_vocab_telex_dict = {x: decompose_to_telex(x) for x in full_vocab}
     a = create_confusion_set(full_vocab_telex_dict, m_edit_distance_1, MAX_SET=50)
     with open('confusion_set.pickle', 'wb') as handle:
